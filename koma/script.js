d5c8327960b98f63ea64eea5f8b6e580ea244bfd
pencarikatakunci <92099290+pencarikatakunci@users.noreply.github.com>
2021-11-06 20:25:26 +0700

    Update script.js

--- koma/script.js
+++ koma/script.js
@@ -2,3 +2,8 @@ $("#btn").click(() => {
   let raw = $("#text").val().split('\n')
   $("#text").val(raw.join(", "))
 }) 
+
+$("#btn").click(() => {
+  let raw = $("#koma").val().split(',')
+  $("#koma").val(raw.join("\n "))
+}) 
