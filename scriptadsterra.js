93ddb7ece8fe4c4bc716102e848f1a17b5043ca5
pencarikatakunci <92099290+pencarikatakunci@users.noreply.github.com>
2021-11-19 08:16:23 +0700

    Create scriptadsterra.js

---
+++ scriptadsterra.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+  
+  'https://www.effectivegatetocontent.com/q77f2xvvh?key=5dbf448426aa07780fb8397b5814ddaa',
+  'https://www.effectivegatetocontent.com/a5ua8fkhjr?key=338e5bbc3bfed85c88e7b9f3d870e144',
+  'https://www.effectivegatetocontent.com/y1pbneas?key=85ccf0896cf3b35fbacf28b0c9b20893', 
+ 
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
